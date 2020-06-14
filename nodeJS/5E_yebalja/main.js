5a90cd18ef0399885de864db815f43bcff83c084
seongsangCHO <secho@student.42seoul.kr>
2020-06-14 18:56:51 +0900

    14 June complete CRUD

--- nodeJS/5E_yebalja/main.js
+++ nodeJS/5E_yebalja/main.js
@@ -5,7 +5,7 @@ var qs = require('querystring');
 var bodyParser = require('body-parser');
 var db = require('./config/db_conn');
 var adminRouter = require('./routes/admin');
-
+var
 app.set('view engine', 'ejs');
 
 app.use('/admin', adminRouter);
@@ -14,12 +14,7 @@ app.use(bodyParser.urlencoded({ extended: true}));
 
 // main page list print
 app.get('/', function (req, res) {
-    db.query('SELECT * FROM program_table',
-        function(err, results, fields){
-           if(err)
-               console.log(err);
-           res.render('main', {list : results});
-        });
+    res.render('main');
 });
 
 
