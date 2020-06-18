4aa5c2956f85bfe5518c4924bc2346f18f83a1f0
seongsangCHO <secho@student.42seoul.kr>
2020-06-18 22:00:46 +0900

    18 June Connect Database and Print data

--- React/server.js
+++ React/server.js
@@ -2,38 +2,18 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 5000;
+const db = require('./db_config');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
 
 app.get('/api/customers', (req, res) => {
-    res.send([
-            {
-              'id' : 1,
-              'image' :'https://placeimg.com/64/64/1',
-              'name' : 'secho',
-              'birthday' : '1125',
-              'gender' : 'man',
-              'job' : 'TT'
-            },
-            {
-              'id' : 2,
-              'image' :'https://placeimg.com/64/64/2',
-              'name' : 'who',
-              'birthday' : '1225',
-              'gender' : 'man',
-              'job' : 'TUT'
-            },
-            {
-              'id' : 3,
-              'image' :'https://placeimg.com/64/64/3',
-              'name' : 'you',
-              'birthday' : '0125',
-              'gender' : 'girl',
-              'job' : 'TOT'
-            }
-    ]);
+    db.query('SELECT * FROM CUSTOMER', function(err, results){
+        if(err)
+            throws(err);
+        res.send(results);
+    });
 });
 
 app.listen(port, ()=> console.log(`Listening on port${port}`));
