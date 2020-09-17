c11812d158b6951f2ccc43a3e461a38c35e94ec3
seongsangCHO <secho@student.42seoul.kr>
2020-09-17 14:49:44 +0900

    JS30 Day02

---
+++ Java Script/JS30/Day02_CSS+JS Clock/script.js
@@ -0,0 +1,38 @@
+const secondHand = document.querySelector(".second-hand");
+const minHand = document.querySelector(".min-hand");
+const hourHand = document.querySelector(".hour-hand");
+
+
+const clockHour = document.querySelector(".hour");
+const clockMin = document.querySelector(".min");
+const clockSecond = document.querySelector(".second");
+console.log(secondHand);
+
+function setTime() {
+  const now = new Date();
+  let second = now.getSeconds();
+  let min = now.getMinutes();
+  let hour = now.getHours();
+  //각도계산
+  //초기 위치에서 90도 이미 이동했으니 offset을 맞춰주기 위해 90추가
+  let secondDegree = (second / 60) * 360 + 90;
+  console.log(second);
+  secondHand.style.transform = `rotate(${secondDegree}deg)`;
+
+
+  let minDegree = (min / 60) * 360 + 90;
+  minHand.style.transform = `rotate(${minDegree}deg)`;
+
+  let hourDegree = (hour / 60) * 360 + 90;
+  hourHand.style.transform = `rotate(${hourDegree}deg)`;
+  
+
+  console.log(clockHour);
+  clockHour.innerHTML = `${hour}` + ':';
+
+  clockMin.innerHTML = `${min}` + ':';
+  clockSecond.innerHTML = `${second}`;
+  
+}
+
+setInterval(setTime, 1000);
