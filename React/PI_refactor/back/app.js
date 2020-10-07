273bac5e90d9020ec2dea52cd68d1127d3af36ca
seongsangCHO <secho@student.42seoul.kr>
2020-10-07 21:03:32 +0900

    using mysql on GCP server show API

--- React/PI_refactor/back/app.js
+++ React/PI_refactor/back/app.js
@@ -1,22 +1,23 @@
-let express = require('express');
-let path = require('path');
+let express = require("express");
+let path = require("path");
 
 const app = express();
 const port = process.env.PORT || 80;
+
+let testAPIRouter = require("./routes/testAPI");
 //템플릿엔진 ejs 설정 __dirname +'views'랑 같음
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
+app.use("/", testAPIRouter);
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.send("hello World");
 });
 
-
-app.get('/api', (req, res) => { 
-  res.render('../views/index', { title: 'api page' });
-})
+app.get("/api", (req, res) => {
+  res.render("../views/index", { title: "api page" });
+});
 app.listen(port, () => {
   console.log(`server is listening at localhost:${port}`);
 });
-
--- React/PI_refactor/back/config/db_config.js
+++ React/PI_refactor/back/config/db_config.js
@@ -1,19 +1,17 @@
 //db 설정 파일
-const mysql = require('mysql');
-require('dotenv').config();
+const mysql = require("mysql");
+require("dotenv").config();
 
-try {
-  const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    port: 3306,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: 'PI_test'
-  })
-} catch (err) { 
-  if (err) console.error(err);
-}
+const connection = mysql.createConnection({
+  host: "34.64.89.203",
+  port: 3306,
+  user: "secho",
+  password: "1234",
+  database: "PI_DB",
+});
 
-connection.connect((err) => { 
+connection.connect((err) => {
   if (err) throw err;
-})
+});
+
+module.exports = connection;
