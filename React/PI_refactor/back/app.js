90c0f9b7c47997b71f7291d190edf82b668c4990
seongsangCHO <secho@student.42seoul.kr>
2020-10-09 00:44:02 +0900

    ssg first Page Crawling

--- React/PI_refactor/back/app.js
+++ React/PI_refactor/back/app.js
@@ -1,10 +1,9 @@
 let express = require("express");
 let path = require("path");
-let crawler = require('./crawler/testcrawler');
+let crawler = require("./crawler/testcrawler");
 
 const app = express();
 const port = process.env.PORT || 80;
-
 
 let testAPIRouter = require("./routes/testAPI");
 
@@ -22,9 +21,22 @@ app.get("/api", (req, res) => {
   res.render("../views/index", { title: "api page" });
 });
 
+//클릭시 처리를 어떻게 해야할까
+//https://stackoverflow.com/questions/55647287/how-to-send-request-on-click-react-hooks-way
 
-app.get('/craw', (req, res) => {
+//클릭데이터를 서버에 전달해야하는뎀.
+
+//크롤러는 하나임, 데이터를 저장할 크롤러 하나 뿐
+//링크박스 추가시 크롤러데이터를 저장할 부분과 클릭할 때 이를 가져올 부분을 따로 작성해야함.
+//링크박스가 저장될때마다 ,요청이 발생할때마다 호출할 함수 crawler
+//링크박스의 제목을 기반으로 크롤러 수행
+//크롤러 데이터를 db에 저장함
+//
+app.get("/craw", (req, res) => {
+  let status = "크롤러수행중";
   crawler();
+
+  res.send(status);
 });
 
 app.listen(port, () => {
--- React/PI_refactor/back/crawler/testcrawler.js
+++ React/PI_refactor/back/crawler/testcrawler.js
@@ -1,6 +1,16 @@
 const puppeteer = require("puppeteer");
 const cheerio = require("cheerio");
 let db = require("../config/db_config");
+let iconv = require("iconv-lite");
+
+function encodeText(str) {
+  let euckrObj = iconv.encode(str, "euc-kr"); //스트링을 euc-kr로 인코딩
+  let result = "";
+  for (const code of euckrObj) {
+    result += "%" + code.toString(16); //인코딩된(euc-kr용 숫자로 바뀐) 숫자를 16진법으로 변환한 뒤 %와 결합해서 URL용 EUC-KR버전으로 바꿈
+  }
+  return result;
+}
 
 //품목기반으로 검색한 크롤링을 해야하는데,
 //크롤링에 인자전달하는 방법
@@ -14,25 +24,47 @@ const crawler = async () => {
   await page.setExtraHTTPHeaders({
     "accept-charset": "euc-kr", //한글 깨지는 문제를 해결해보려고 charset을 바꿔봤는데 해결안됨
   });
+  //링크 title을 요청받아와서 사용
+  let searchText = "물";
 
-  let name = {
-    user: [{}],
-  };
-  try {
-    let sql = `select * from testAPI`;
+  let encodedSearchText = encodeText(searchText);
+  await page.setDefaultNavigationTimeout(0);
+  await page.goto(
+    `http://www.ssg.com/search.ssg?target=all&query=${searchText}`,
+    // http://www.ssg.com/search.ssg?target=all&query=%EB%AC%BC&sort=sale 판매량순
+    { waitUntil: "networkidle0" }
+  );
 
-    db.query(sql, (error, result) => {
-      if (error) console.error(error);
-      // console.log(result);
-      const move = async (result) => {
-        await page.goto(`https://google.com?${result[0].user_id}`);
-      };
-      move(result);
-    });
-  } catch (error) {
-    if (error) console.error(error);
+  const ulContentSelector = `#divProductImg > #idProductImg li`;
+
+  const liLength = await page.evaluate((SELECTOR) => {
+    //page당 아이템 갯수 출력 80개
+    return document.querySelectorAll(SELECTOR).length;
+  }, ulContentSelector);
+
+  let productData = [];
+  for (let idx = 1; idx <= liLength; idx++) {
+    let productObj = {};
+    productObj["title"] = await page.$eval(
+      `#idProductImg li:nth-child(${idx}) div.title a em.tx_ko`,
+      (element) => element.textContent
+    );
+
+    productObj["price"] = await page.$eval(
+      `#idProductImg li:nth-child(${idx}) em.ssg_price`,
+      (element) => element.textContent + "원"
+    );
+    productObj["link"] = await page.$eval(
+      `#idProductImg li:nth-child(${idx}) div.title a`,
+      (element) => element.href
+    );
+    productData.push(productObj);
   }
-  console.log(name);
+  console.log(productData);
+
+  console.log("Load 끝");
+  await page.close(); // 페이지 닫기
+  await browser.close(); // 브라우저 닫기
 };
 
 module.exports = crawler;
