b41d343b91f33841f5c3220d57b4a33ead844145
seongsangCHO <secho@student.42seoul.kr>
2020-10-09 17:41:50 +0900

    headless true mode for server deploy , add page userAgent

--- React/PI_refactor/back/crawler/coupangCrawler.js
+++ React/PI_refactor/back/crawler/coupangCrawler.js
@@ -3,19 +3,27 @@ const cheerio = require("cheerio");
 let db = require("../config/db_config");
 let iconv = require("iconv-lite");
 var assert = require("assert");
+const { pathToFileURL } = require("url");
 
 const coupangCrawler = async () => {
   let start = await new Date().getTime();
   //Common part//
-  const browser = await puppeteer.launch({ headless: false });
+  const browser = await puppeteer.launch({ headless: true });
   await browser.userAgent(
     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36"
   );
+
   const page = await browser.newPage();
+  process.on("unhandledRejection", (reason, p) => {
+    console.error("Unhandled Rejection at: Promise", p, "reason:", reason);
+    browser.close();
+  });
+  await page.setUserAgent(
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36"
+  );
   await page.setExtraHTTPHeaders({
     "accept-charset": "euc-kr", //한글 깨지는 문제를 해결해보려고 charset을 바꿔봤는데 해결안됨
   });
-  await page.setRequestInterception(true);
   page.on("request", (request) => {
     if (request.resourceType() === "font") request.abort();
     else request.continue();
@@ -26,19 +34,33 @@ const coupangCrawler = async () => {
   //이를 외래키로 지정해서 하위 데이터들을 추가시켜주어야하네..a1
 
   //시간제한 없애기
-  await page.setDefaultNavigationTimeout(0);
-  //동시에 여러 페이지 newPage로 띄워서  promise.all로 각각 페이지를 모듈로 나눠서 크롤링실행해야겠다.
-  await page.goto(
-    `https://www.coupang.com/np/search?q=${searchText}&channel=user&component=&eventCategory=SRP&trcid=&traid=&sorter=scoreDesc&minPrice=&maxPrice=&priceRange=&filterType=&listSize=72&filter=&isPriceRange=false&brand=&offerCondition=&rating=0&page=1&rocketAll=false&searchIndexingToken=1=4&backgroundColor=`,
-    //page로 넘기면 검색가능
-    { waitUntil: "networkidle2" }
-  );
+  try {
+    await page.setRequestInterception(true);
+
+    await page.setDefaultNavigationTimeout(0);
+    //동시에 여러 페이지 newPage로 띄워서  promise.all로 각각 페이지를 모듈로 나눠서 크롤링실행해야겠다.
+    await page.goto(
+      `https://www.coupang.com/np/search?q=${searchText}&channel=user&component=&eventCategory=SRP&trcid=&traid=&sorter=scoreDesc&minPrice=&maxPrice=&priceRange=&filterType=&listSize=72&filter=&isPriceRange=false&brand=&offerCondition=&rating=0&page=1&rocketAll=false&searchIndexingToken=1=4&backgroundColor=`,
+      //page로 넘기면 검색가능
+      { waitUntil: "networkidle2" }
+    );
+  } catch (error) {
+    console.error(error);
+  }
+
   //Common part//
 
-  //lastPage넘버 set
-  const lastPageNumber = await page.$eval(`.btn-last`, (element) => {
-    return element.textContent;
-  });
+  // lastPage넘버 set
+  try {
+    await page.waitForSelector("div.search-pagination a.btn-last.disabled");
+  } catch (error) {
+    console.error(error);
+  }
+
+  const lastPageNumber = await page.$eval(
+    "div.search-pagination a.btn-last.disabled",
+    (element) => element.textContent
+  );
 
   console.log(lastPageNumber);
 
@@ -59,6 +81,9 @@ const coupangCrawler = async () => {
           { waitUntil: "networkidle2" }
         );
       }
+      await page.addScriptTag({
+        url: "https://code.jquery.com/jquery-3.2.1.min.js",
+      });
       //페이지당 광고 삭제
       await page.evaluate(() => {
         $(`.search-product__ad-badge`).remove();
@@ -94,6 +119,7 @@ const coupangCrawler = async () => {
         if (productObj.title && productObj.price && productObj.link)
           productData.push(productObj);
         else priority--;
+        console.log(productObj);
       }
     }
   } catch (error) {
@@ -101,6 +127,7 @@ const coupangCrawler = async () => {
   }
   let end = await new Date().getTime();
   console.log("쿠팡크롤러 time :" + (end - start) / 1000);
+
   await page.close(); // 페이지 닫기
   await browser.close(); // 브라우저 닫기
 };
--- React/PI_refactor/parse.js
+++ React/PI_refactor/parse.js
@@ -60,3 +60,70 @@ function findCategory() {
 }
 
 findCategory();
+
+
+
+const ssgCrawler = async (page, searchText) => {
+  try {
+    await page.setDefaultNavigationTimeout(0);
+
+    const ulContentSelector = `#divProductImg > #idProductImg li`;
+
+    const liLength = await page.evaluate((SELECTOR) => {
+      //page당 아이템 갯수 출력 80개
+      return document.querySelectorAll(SELECTOR).length;
+    }, ulContentSelector);
+    if (liLength <= 0) {
+      //li 태그의 길이가 없다 == 검색결과 없을때 크롤링종료
+      console.log("ssg_ li length is zero");
+    } else {
+      //마지막 페이지 번호를 구함
+      const lastPageNumber = await page.$eval(`.btn_last`, (element) => {
+        return element.getAttribute("data-filter-value").split("=")[1];
+      });
+      try {
+        let productData = [];
+
+        //첫페이지 ~ 3페이지까지 크롤링
+        //맨처음 들어오는 값이 page=1임,
+
+        for (
+          let pageNumber = 1;
+          pageNumber <= lastPageNumber - (lastPageNumber - 3);
+          pageNumber++
+        ) {
+          if (pageNumber != 1) {
+            await page.goto(
+              `http://www.ssg.com/search.ssg?target=all&query=${searchText}&page=${pageNumber}`,
+              { waitUntil: "networkidle2" }
+            );
+          }
+          for (let idx = 1; idx <= liLength; idx++) {
+            let productObj = {};
+            productObj["priority"] = idx + (pageNumber - 1) * liLength;
+
+            productObj["title"] = await page.$eval(
+              `#idProductImg li:nth-child(${idx}) div.title a em.tx_ko`,
+              (element) => element.textContent
+            );
+
+            productObj["price"] = await page.$eval(
+              `#idProductImg li:nth-child(${idx}) em.ssg_price`,
+              (element) => element.textContent + "원"
+            );
+            productObj["link"] = await page.$eval(
+              `#idProductImg li:nth-child(${idx}) div.title a`,
+              (element) => element.href
+            );
+            productData.push(productObj);
+          }
+        }
+        ssgDataInsert(productData);
+      } catch (error) {
+        if (error) console.error(error);
+      }
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
