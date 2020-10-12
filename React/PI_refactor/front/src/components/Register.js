ec5fa10e249ed0c8caa2d80bd0bec377d8e40819
seongsangCHO <secho@student.42seoul.kr>
2020-10-12 15:00:08 +0900

    React route

--- React/PI_refactor/front/src/components/Register.js
+++ React/PI_refactor/front/src/components/Register.js
@@ -1,8 +1,28 @@
 import React from "react";
 import Header from "./Header";
+import styled from "styled-components";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
+const RegisterWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+`;
+const NickInput = styled.input`
+  width: 100% type= "text";
+`;
 function Register() {
-  return (<div className="Register">뭐어쩌라고</div>);
+  return (
+    <RegisterWrapper>
+      <form>
+        <div>
+          <label>닉네임</label>
+        </div>
+        <NickInput></NickInput>
+      </form>
+    </RegisterWrapper>
+  );
 }
 
 export default Register;
