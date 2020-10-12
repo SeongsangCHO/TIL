f01ab0ca5fa05187199cf9f16c96c842a6b377c0
seongsangCHO <secho@student.42seoul.kr>
2020-10-12 15:38:16 +0900

    double check 해야함

--- React/PI_refactor/front/src/components/Register.js
+++ React/PI_refactor/front/src/components/Register.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./Header";
 import styled from "styled-components";
 import Form from "react-bootstrap/Form";
@@ -10,16 +10,109 @@ const RegisterWrapper = styled.div`
   justify-content: center;
 `;
 const NickInput = styled.input`
-  width: 100% type= "text";
+  width: fit-content;
+  border: 1px solid #000;
+  margin-bottom: 15px;
+`;
+const PasswordInput = styled.input`
+  width: fit-content;
+  border: 1px solid #000;
+  margin-bottom: 15px;
+`;
+const PasswordCheck = styled.input`
+  width: fit-content;
+
+  border: 1px solid #000;
+  margin-bottom: 15px;
+`;
+
+const DoubleCheck = styled.button`
+  background-color: lavender;
+`;
+
+const RegisterButton = styled.button`
+  background-color: lavender;
 `;
 function Register() {
+  const [nickName, setNickName] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordCheck, setPasswordCheck] = useState(false);
+  const handlePasswordInput = (e) => {
+    setPassword(e.target.value);
+    console.log(password);
+  };
+
+  const handlePasswordCheck = (e) => {
+    let doubleCheckValue = e.target.value;
+    if (doubleCheckValue == "") {
+      setPasswordCheck(false);
+      return false;
+    }
+    if (doubleCheckValue == password) {
+      setPasswordCheck(true);
+      console.log("비밀번호 일치");
+    }
+    return false;
+  };
+
+  const handleSubmit = (e) => {
+    if (passwordCheck == false && setPassword != "") {
+      console.log("비번이 공백이거나 일치하지 않음");
+      alert("비밀번호가 공백이거나 일치하지 않아요");
+      e.preventDefault();
+      return false;
+    }
+    return true;
+  };
+  const handleNickName = (e) => {
+    setNickName(e.target.value);
+    console.log(e.target.value);
+  };
+  const getNickName = (e) => {
+    console.log(nickName);
+    //여기서 서버에서 가져온 데이터랑 중복체크
+  };
   return (
     <RegisterWrapper>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
           <label>닉네임</label>
         </div>
-        <NickInput></NickInput>
+        <NickInput
+          onChange={handleNickName}
+          id="nick"
+          type="text"
+          placeholder="닉네임입력"
+        ></NickInput>
+        <DoubleCheck onClick={getNickName} type="button">
+          {" "}
+          닉네임 중복체크{" "}
+        </DoubleCheck>
+        <div>
+          <label>패스워드</label>
+        </div>
+        <PasswordInput
+          type="password"
+          onChange={handlePasswordInput}
+          placeholder="비밀번호"
+        ></PasswordInput>{" "}
+        <div>
+          <label>패스워드 확인</label>
+        </div>
+        <PasswordCheck
+          onFocus={handlePasswordCheck}
+          onChange={handlePasswordCheck}
+          type="password"
+          placeholder="비밀번호 확인"
+        ></PasswordCheck>
+        {passwordCheck == true ? (
+          <label>패스워드 일치</label>
+        ) : (
+          <label>패스워드 확인해주세요.</label>
+        )}
+        <div>
+          <RegisterButton type="submit">가입</RegisterButton>
+        </div>
       </form>
     </RegisterWrapper>
   );
